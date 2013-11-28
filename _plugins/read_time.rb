1ecaaf7ca35e7165540961ac053587a6244dce76
Ray Clanan <rclanan@utopianconcept.com>
2013-11-27 23:10:44 -0500

    Add Infinity Site Launch Post

--- _plugins/read_time.rb
+++ _plugins/read_time.rb
@@ -1,16 +1,49 @@
-module ReadTimeFilter
+# reading_time
+#
+# A Liquid filter to estimate how long a passage of text will take to read
+#
+# https://github.com/bdesham/reading_time
+#
+# Copyright (c) Benjamin Esham, 2013
+# See README.md for full copyright information.
 
-  def readtime(input)
+require 'nokogiri'
 
-    charcount = 4.5
-    wpm = 180
+module Jekyll
+        module ReadingTime
 
-    rt = (input.to_f/charcount/wpm).round
-    rt = 1 if rt < 1
-    rt
+                def count_words(html)
+                        words(html).length
+                end
 
-  end
+                def reading_time(html)
+                        (count_words(html) / 270.0).ceil
+                end
 
-  Liquid::Template.register_filter self
+                private
 
+                def text_nodes(root)
+                        ignored_tags = %w[ area audio canvas code embed footer form img
+                                map math nav object pre script svg table track video ]
+
+                        texts = []
+                        root.children.each { |node|
+                                if node.text?
+                                        texts << node.text
+                                elsif not ignored_tags.include? node.name
+                                        texts.concat text_nodes node
+                                end
+                        }
+                        texts
+                end
+
+
+                def words(html)
+                        fragment = Nokogiri::HTML.fragment html
+                        text_nodes(fragment).map { |text| text.scan(/[[:word:]]+/)}.flatten
+                end
+
+        end
 end
+
+Liquid::Template.register_filter(Jekyll::ReadingTime)
